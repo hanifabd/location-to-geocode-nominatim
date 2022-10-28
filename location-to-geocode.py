aa0515693ae61fd4c88ab571bb1dfa540bde0904
Hanif Yuli Abdillah P <hanif.abdillah@jatis.com>
2022-10-28 14:52:28 +0700

    add error handle

--- location-to-geocode.py
+++ location-to-geocode.py
@@ -4,7 +4,12 @@ from geopy.geocoders import Nominatim
 def get_geocode(location_name):
     geolocator = Nominatim(user_agent="myGeocoder")
     
-    address_string = "bangkle"
+    address_string = location_name
     location = geolocator.geocode(address_string)
 
-    return location.raw
+    if location != None:
+        return location.raw
+    else:
+        return 404
+
+print(get_geocode("bangkle blora"))
